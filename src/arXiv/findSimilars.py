bfc6f26ab1f916dac24847a07fb9ecd8e377933c
Giannis Clipper <giannisclipper@gmail.com>
2025-03-30 15:47:25 +0300

    Work on cumulative gain factors.

--- src/arXiv/findSimilars.py
+++ src/arXiv/findSimilars.py
@@ -1,6 +1,7 @@
 import pickle
 import time
 import random
+import math
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity # type: ignore
 
@@ -67,7 +68,7 @@ random_record = random.randint( 0, corpus_repr.shape[0]-1 )
 start_time = time.time()
 random_repr = corpus_repr[ random_record ]
 
-random_repr = preprocessor.transform( [ "Literature about DB SQL semantics?" ] )
+random_repr = preprocessor.transform( [ "Literature about databases (both SQL and NoSQL) and if they are somehow related to semantics?" ] )
 random_repr = vectorizer.transform( random_repr )
 
 similarities = compute_similarities( random_repr, corpus_repr )
@@ -78,3 +79,55 @@ similarities = [ ( x[0][ 'id' ], x[0][ 'catg_ids' ], x[1] ) for x in zip( corpus
 similarities.sort( key=lambda x: x[2], reverse=True )
 for i in range( 20 ):
     print( similarities[ i ] )
+
+# #####################################################
+
+gains = [ s[2] for s in similarities[:20] ]
+random.shuffle( gains )
+
+def cumulative_gain( gains:list[float] ):
+    counter = 0
+    result = []
+    for i in range( len( gains ) ):
+        counter += gains[ i ]
+        result.append( counter )
+    return result
+
+cg = cumulative_gain( gains )
+# print( 'CG:', cg )
+
+def discounted_cumulative_gain( gains:list[float] ):
+    counter = 0.0
+    result = []
+    for i, rel in enumerate( gains ):
+        rank = i + 1  # ranks start at 1
+        counter += rel / math.log2( rank + 1 )  # discount factor: log2(rank+1)
+        result.append( counter )
+    return result
+
+dcg = discounted_cumulative_gain( gains )
+# print( 'DCG:', dcg )
+
+def ideal_discounted_cumulative_gain( gains:list[float] ):
+    # sort gains in descending order for ideal ranking
+    sorted_gains = sorted( gains, reverse=True )
+    return discounted_cumulative_gain( sorted_gains )
+
+idcg = ideal_discounted_cumulative_gain( gains )
+# print( 'IDCG:', idcg )
+
+def normalized_discounted_cumulative_gain( gains:list[float] ):
+    # sort gains in descending order for ideal ranking
+    dcg = discounted_cumulative_gain( gains )
+    idcg = ideal_discounted_cumulative_gain( gains )
+    return [ x[0] / x[1] for x in zip( dcg, idcg ) ]
+
+ndcg = normalized_discounted_cumulative_gain( gains )
+# print( 'NDCG:', ndcg )
+
+print()
+print( "------", "------", "------", "------", "------","------" )
+print( "RANK  ", "REL   ", "CG    ", "DCG   ", "IDCG  ","NDCG  " )
+print( "------", "------", "------", "------", "------","------" )
+for i, g0, g1, g2, g3, g4 in zip( range(1, len(cg)+1), gains, cg, dcg, idcg, ndcg ):
+    print( f' {i:3} {g0:6.2} {g1:6.2} {g2:6.2} {g3:6.2} {g4:6.2}')
