2ecbfcafd34a05d5f25ca842556b4ce8cb4e9e44
Giannis Clipper <giannisclipper@gmail.com>
2025-04-08 06:00:46 +0300

    Count authors as unique names.

---
+++ src/arXiv/countAuthors.py
@@ -0,0 +1,15 @@
+
+from Dataset import Dataset
+
+print( f'Load corpus...' )
+ds = Dataset()
+records = ds.toDictList()
+authors = []
+for i in range( len( records ) ):
+    authors += records[ i ][ 'authors' ]
+
+print( 'All instances:', len( authors ) )
+print( 'Unique names:', len( set( authors ) ) )
+print( authors[:10] )
+
+print( [ a for a in authors if 'Taylor' in a ] )
