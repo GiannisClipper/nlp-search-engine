238fd78d88b33d796383f627c66deaf5ccd1f152
Giannis Clipper <giannisclipper@gmail.com>
2025-04-21 10:23:08 +0300

    Update makeVectorizer (use both CountVectorizer and TfidfVectorizer).

--- src/arXiv/makeVectorizer.py
+++ src/arXiv/makeVectorizer.py
@@ -1,58 +1,106 @@
-import time
 import pickle
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer # type: ignore
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-import nltk # type: ignore
-nltk.download( 'stopwords' )
-nltk.download( 'wordnet' )
+from .Dataset import Dataset
+from .Preprocessor import Preprocessor
+from .Preprocessor import LowerConverter, PunctRemover, SpecialsRemover
+from .Preprocessor import StopwordsRemover, Stemmer, Lemmatizer
+from .Tokenizer import SingleTokenizer
+from .settings import pickle_filenames
 
-from nltk.corpus import stopwords # type: ignore
-from nltk.stem import SnowballStemmer # type: ignore
-from nltk.stem import WordNetLemmatizer # type: ignore
+from .helpers.decorators import with_time_counter
 
-from Dataset import Dataset
-from Preprocessor import Preprocessor
-from Preprocessor import LowerConverter, PunctRemover, SpecialsRemover
-from Preprocessor import StopwordsRemover, Stemmer, Lemmatizer
+# +------------------+
+# | Load the dataset |
+# +------------------+
 
-print( f'Load corpus...' )
+print( f'\nLoad corpus...' )
 ds = Dataset()
 records = ds.toDictList()
 corpus = []
 for i in range( len( records ) ):
     corpus.append( records[ i ][ 'title' ] + '-' + records[ i ][ 'summary' ] )
 
-print( f'Preprocessing...' )
+# +---------------------------+
+# | Perform the preprocessing |
+# +---------------------------+
+
+print( f'\nPreprocessing...' )
 preprocessor = Preprocessor( [
     LowerConverter(), 
     PunctRemover(), 
     SpecialsRemover(),
     StopwordsRemover(), 
-    Stemmer(), 
+    Stemmer(),
     # Lemmatizer()
 ] )
+
 corpus = preprocessor.transform( corpus )
 print( 'corpus[::1000]:', corpus[::1000] )
 print()
 
-print( f'Make vectorizer...', end=' ' )
-start_time = time.time()
-vectorizer = TfidfVectorizer( max_features=500000 )
-corpus_repr = vectorizer.fit_transform( corpus )
-print( f'({round(time.time()-start_time, 1)} secs)' )
-
-vocabulary = vectorizer.get_feature_names_out()
-print( 'Vocabulary[::500]:', len( vocabulary ), vocabulary[::500] )
-print()
-
-print( 'Save preprocessor...' )
-with open( 'pickles/preprocessor.pkl', 'wb' ) as f:
+print( 'Saving preprocessor in disk...' )
+with open( pickle_filenames[ 'preprocessor' ], 'wb' ) as f:
     pickle.dump( preprocessor, f )
 
-print( 'Save vectorizer...' )
-with open( 'pickles/vectorizer.pkl', 'wb' ) as f:
+# +-----------------------+
+# | Create the vocabulary |
+# +-----------------------+
+
+@with_time_counter
+def create_vocabulary( message=None, *args, **kwargs ):
+    return SingleTokenizer( ' '.join( corpus ) ).tokens
+
+vocabulary = create_vocabulary( '\nCreating vocabulary...' )
+
+print( '\nVocabulary[::500]:', len( vocabulary ), vocabulary[::500] )
+
+print( 'Saving vocabulary in disk...' )
+with open( pickle_filenames[ 'vocabulary' ], 'wb' ) as f:
+    pickle.dump( vocabulary, f )
+
+# +---------------------------+
+# | Create a count vectorizer |
+# +---------------------------+
+
+@with_time_counter
+def create_count_vectorizer( message=None, *args, **kwargs ):
+    vectorizer = CountVectorizer( vocabulary=vocabulary )
+    corpus_repr = vectorizer.fit_transform( corpus )
+    return vectorizer, corpus_repr
+
+vectorizer, corpus_repr = create_count_vectorizer( '\nCreating count vectorizer...' )
+
+print( '\nSaving count vectorizer in disk...' )
+with open( pickle_filenames[ 'count' ][ 'vectorizer' ], 'wb' ) as f:
     pickle.dump( vectorizer, f )
 
-print( 'Save corpus representations...' )
-with open( 'pickles/corpus_repr.pkl', 'wb' ) as f:
+print( 'Saving corpus representations in disk...' )
+with open( pickle_filenames[ 'count' ][ 'corpus_repr' ], 'wb' ) as f:
     pickle.dump( corpus_repr, f )
+
+# +---------------------------+
+# | Create a tfidf vectorizer |
+# +---------------------------+
+
+@with_time_counter
+def create_tfidf_vectorizer( message=None, *args, **kwargs ):
+    # vectorizer = TfidfVectorizer( max_features=5000 )
+    vectorizer = TfidfVectorizer( vocabulary=vocabulary )
+    corpus_repr = vectorizer.fit_transform( corpus )
+    return vectorizer, corpus_repr
+
+vectorizer, corpus_repr = create_tfidf_vectorizer( '\nCreating tfidf vectorizer...' )
+
+print( '\nSaving tfidf vectorizer in disk...' )
+with open( pickle_filenames[ 'tfidf' ][ 'vectorizer' ], 'wb' ) as f:
+    pickle.dump( vectorizer, f )
+
+print( 'Saving corpus representations in disk...' )
+with open( pickle_filenames[ 'tfidf' ][ 'corpus_repr' ], 'wb' ) as f:
+    pickle.dump( corpus_repr, f )
+
+# extract the vocabulary
+# vocabulary = vectorizer.get_feature_names_out()
+# print( 'Vocabulary[::500]:', len( vocabulary ), vocabulary[::500] )
+# print()
--- src/arXiv/settings.py
+++ src/arXiv/settings.py
@@ -1,8 +1,25 @@
 import os 
 
 file_path = os.path.dirname( os.path.realpath( __file__ ) )
+
 catgs_filename = f'{file_path}/downloads/categories.txt'
 dataset_filename = f'{file_path}/datasets/dataset.jsonl'
+
+pickle_filenames = {
+    'preprocessor': f'{file_path}/pickles/preprocessor.pkl',
+
+    'vocabulary': f'{file_path}/pickles/vocabulary.pkl',
+
+    'count': {
+        'vectorizer': f'{file_path}/pickles/count_vectorizer.pkl',
+        'corpus_repr': f'{file_path}/pickles/count_corpus_repr.pkl'
+    },
+
+    'tfidf': {
+        'vectorizer': f'{file_path}/pickles/tfidf_vectorizer.pkl',
+        'corpus_repr': f'{file_path}/pickles/tfidf_corpus_repr.pkl'
+    }
+}
 
 # to select specific categories (not all)
 catgs_filter = [ 'AI', 'CL', 'DB', 'DC', 'DS', 'GL', 'IR', 'LG', 'NI', 'PL' , 'SE', 'SI' ]
