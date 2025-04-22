3f766bb67a3e562a4f75c1707d78c4479c5d69bf
Giannis Clipper <giannisclipper@gmail.com>
2025-04-22 15:45:26 +0300

    Update VectorizerMaker to support medical dataset.

--- src/VectorizerMaker.py
+++ src/VectorizerMaker.py
@@ -1,7 +1,6 @@
 import sys
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-from .CorpusLoader import CorpusLoader, TitleSummaryCorpusLoader
 from .Preprocessor import Preprocessor, LemmPreprocessor, StemmPreprocessor
 
 from .helpers.decorators import with_time_counter
@@ -11,20 +10,19 @@ class VectorizerMaker:
 
     def __init__( 
             self, 
-            corpusLoader:CorpusLoader, 
+            corpus:list[str], 
             preprocessor:Preprocessor, 
             vocabularyLoader:PickleLoader, 
             VectorizerClass:type[CountVectorizer|TfidfVectorizer]
         ):
-        self._corpusLoader = corpusLoader
+        self._corpus = corpus
         self._preprocessor = preprocessor
         self._vocabularyLoader = vocabularyLoader
         self._VectorizerClass = VectorizerClass
 
     def make( self ):
         print( f'\nPreprocessing...' )
-        corpus = self._corpusLoader.load()
-        corpus = self._preprocessor.transform( corpus )
+        corpus = self._preprocessor.transform( self._corpus )
         vocabulary = self._vocabularyLoader.load()
 
         @with_time_counter
@@ -36,103 +34,104 @@ class VectorizerMaker:
         return create_vectorizer( '\nCreating vectorizer...' )
 
     def __str__( self ):
-        return self.__class__
+        return self.__class__    
 
 
-# RUN: python -m src.VectorizerMaker [parameter]
+def make_and_save( 
+    pickle_paths:dict,
+    vocabulary_descr:str, 
+    vectorizer_descr:str, 
+    corpus:list[str], 
+    PreprocessorClass, 
+    VectorizerClass
+):
+
+    vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_descr}.pkl"
+
+    vectorizerMaker = VectorizerMaker(
+        corpus,
+        PreprocessorClass(),
+        PickleLoader( vocabulary_filename ),
+        VectorizerClass
+    )
+    vectorizer, corpus_repr = vectorizerMaker.make()
+
+    vectorizer_filename = f"{pickle_paths[ 'vectorizers' ]}/{vectorizer_descr}.pkl"
+    PickleSaver( vectorizer_filename ).save( vectorizer )
+
+    # common description with vectorizer
+    corpus_repr_filename = f"{pickle_paths[ 'corpus_repr' ]}/{vectorizer_descr}.pkl"
+    PickleSaver( corpus_repr_filename ).save( corpus_repr )
+
+
+# RUN: python -m src.VectorizerMaker [option]
 if __name__ == "__main__": 
 
-    from .arXiv.settings import pickle_paths
-
     option = None
-    if len( sys.argv ) > 1:
+    if len( sys.argv ) >= 2:
         option = sys.argv[ 1 ]
 
     match option:
 
-        case 'stemm-count':
-
-            vocabulary_descr = 'title-summary_lower-punct-specials-stops-stemm_single'
-            vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_descr}.pkl"
-
-            vectorizer_descr = 'title-summary_lower-punct-specials-stops-stemm_single_count'
-            vectorizerMaker = VectorizerMaker(
-                TitleSummaryCorpusLoader(),
-                StemmPreprocessor(),
-                PickleLoader( vocabulary_filename ),
-                CountVectorizer
+        case 'arxiv-stemm-single-count':
+            from .arXiv.Dataset import Dataset
+            from .arXiv.settings import pickle_paths
+            make_and_save( 
+                pickle_paths, 
+                vocabulary_descr='title-summary_lower-punct-specials-stops-stemm_single', 
+                vectorizer_descr='title-summary_lower-punct-specials-stops-stemm_single_count', 
+                corpus=Dataset().toListTitlesSummaries(), 
+                PreprocessorClass=StemmPreprocessor,
+                VectorizerClass=CountVectorizer
             )
-            vectorizer, corpus_repr = vectorizerMaker.make()
 
-            vectorizer_filename = f"{pickle_paths[ 'vectorizers' ]}/{vectorizer_descr}.pkl"
-            PickleSaver( vectorizer_filename ).save( vectorizer )
-
-            # common description with vectorizer
-            corpus_repr_filename = f"{pickle_paths[ 'corpus_repr' ]}/{vectorizer_descr}.pkl"
-            PickleSaver( corpus_repr_filename ).save( corpus_repr )
-
-        case 'stemm-tfidf':
-
-            vocabulary_descr = 'title-summary_lower-punct-specials-stops-stemm_single'
-            vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_descr}.pkl"
-
-            vectorizer_descr = 'title-summary_lower-punct-specials-stops-stemm_single_tfidf'
-            vectorizerMaker = VectorizerMaker(
-                TitleSummaryCorpusLoader(),
-                StemmPreprocessor(),
-                PickleLoader( vocabulary_filename ),
-                TfidfVectorizer
+        case 'arxiv-stemm-single-tfidf':
+            from .arXiv.Dataset import Dataset
+            from .arXiv.settings import pickle_paths
+            make_and_save( 
+                pickle_paths, 
+                vocabulary_descr='title-summary_lower-punct-specials-stops-stemm_single', 
+                vectorizer_descr='title-summary_lower-punct-specials-stops-stemm_single_tfidf', 
+                corpus=Dataset().toListTitlesSummaries(), 
+                PreprocessorClass=StemmPreprocessor,
+                VectorizerClass=TfidfVectorizer
             )
-            vectorizer, corpus_repr = vectorizerMaker.make()
 
-            vectorizer_filename = f"{pickle_paths[ 'vectorizers' ]}/{vectorizer_descr}.pkl"
-            PickleSaver( vectorizer_filename ).save( vectorizer )
-
-            # common description with vectorizer
-            corpus_repr_filename = f"{pickle_paths[ 'corpus_repr' ]}/{vectorizer_descr}.pkl"
-            PickleSaver( corpus_repr_filename ).save( corpus_repr )
-
-        case 'lemm-count':
-
-            vocabulary_descr = 'title-summary_lower-punct-specials-stops-lemm_single'
-            vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_descr}.pkl"
-
-            vectorizer_descr = 'title-summary_lower-punct-specials-stops-lemm_single_count'
-            vectorizerMaker = VectorizerMaker(
-                TitleSummaryCorpusLoader(),
-                LemmPreprocessor(),
-                PickleLoader( vocabulary_filename ),
-                CountVectorizer
+        case 'arxiv-lemm-single-count':
+            from .arXiv.Dataset import Dataset
+            from .arXiv.settings import pickle_paths
+            make_and_save( 
+                pickle_paths, 
+                vocabulary_descr='title-summary_lower-punct-specials-stops-lemm_single', 
+                vectorizer_descr='title-summary_lower-punct-specials-stops-lemm_single_count', 
+                corpus=Dataset().toListTitlesSummaries(), 
+                PreprocessorClass=LemmPreprocessor,
+                VectorizerClass=CountVectorizer
             )
-            vectorizer, corpus_repr = vectorizerMaker.make()
 
-            vectorizer_filename = f"{pickle_paths[ 'vectorizers' ]}/{vectorizer_descr}.pkl"
-            PickleSaver( vectorizer_filename ).save( vectorizer )
-
-            # common description with vectorizer
-            corpus_repr_filename = f"{pickle_paths[ 'corpus_repr' ]}/{vectorizer_descr}.pkl"
-            PickleSaver( corpus_repr_filename ).save( corpus_repr )
-
-        case 'lemm-tfidf':
-
-            vocabulary_descr = 'title-summary_lower-punct-specials-stops-lemm_single'
-            vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_descr}.pkl"
-
-            vectorizer_descr = 'title-summary_lower-punct-specials-stops-lemm_single_tfidf'
-            vectorizerMaker = VectorizerMaker(
-                TitleSummaryCorpusLoader(),
-                LemmPreprocessor(),
-                PickleLoader( vocabulary_filename ),
-                TfidfVectorizer
+        case 'arxiv-lemm-single-tfidf':
+            from .arXiv.Dataset import Dataset
+            from .arXiv.settings import pickle_paths
+            make_and_save( 
+                pickle_paths, 
+                vocabulary_descr='title-summary_lower-punct-specials-stops-lemm_single', 
+                vectorizer_descr='title-summary_lower-punct-specials-stops-lemm_single_tfidf', 
+                corpus=Dataset().toListTitlesSummaries(), 
+                PreprocessorClass=LemmPreprocessor,
+                VectorizerClass=TfidfVectorizer
             )
-            vectorizer, corpus_repr = vectorizerMaker.make()
 
-            vectorizer_filename = f"{pickle_paths[ 'vectorizers' ]}/{vectorizer_descr}.pkl"
-            PickleSaver( vectorizer_filename ).save( vectorizer )
-
-            # common description with vectorizer
-            corpus_repr_filename = f"{pickle_paths[ 'corpus_repr' ]}/{vectorizer_descr}.pkl"
-            PickleSaver( corpus_repr_filename ).save( corpus_repr )
+        case 'medical-lemm-single-tfidf':
+            from .medical.Dataset import Dataset
+            from .medical.settings import pickle_paths
+            make_and_save( 
+                pickle_paths,
+                vocabulary_descr='title-summary_lower-punct-specials-stops-lemm_single', 
+                vectorizer_descr='title-summary_lower-punct-specials-stops-lemm_single_tfidf', 
+                corpus=Dataset().toListTitlesAbstracts(), 
+                PreprocessorClass=LemmPreprocessor,
+                VectorizerClass=TfidfVectorizer
+            )
 
         case _:
-            raise Exception( 'No valid parameters passed.' )
+            raise Exception( 'No valid option.' )
