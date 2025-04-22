a1d72f4271cf118a4fd3bed53c5d53b81fb2d722
Giannis Clipper <giannisclipper@gmail.com>
2025-04-22 15:31:13 +0300

    Update VocabularyMaker.

--- src/VocabularyMaker.py
+++ src/VocabularyMaker.py
@@ -1,9 +1,6 @@
 import sys
-from abc import ABC, abstractmethod
 
-from .arXiv.CorpusLoader import CorpusLoader
 from .Preprocessor import Preprocessor, LemmPreprocessor, StemmPreprocessor
-
 from .TokenMaker import TokenMaker, SingleTokenMaker
 
 from .helpers.decorators import with_time_counter
@@ -11,15 +8,19 @@ from .helpers.Pickle import PickleSaver
 
 class VocabularyMaker:
 
-    def __init__( self, corpusLoader:CorpusLoader, preprocessor:Preprocessor, tokenMaker:TokenMaker ):
-        self._corpusLoader = corpusLoader
+    def __init__( 
+            self, 
+            corpus:list[str], 
+            preprocessor:Preprocessor, 
+            tokenMaker:TokenMaker 
+        ):
+        self._corpus = corpus
         self._preprocessor = preprocessor
         self._tokenMaker = tokenMaker
 
     def make( self ) -> tuple[str,...]:
         print( f'\nPreprocessing...' )
-        corpus = self._corpusLoader.load()
-        corpus = self._preprocessor.transform( corpus )
+        corpus = self._preprocessor.transform( self._corpus )
         step = len( corpus ) // 5 if len( corpus ) > 5 else 1 
         print( f'corpus[::{step}]:', corpus[::step] )
 
@@ -39,78 +40,61 @@ class VocabularyMaker:
         return self.__class__
 
 
-# RUN: python -m src.VocabularyMaker [parameter]
-if __name__ == "__main__": 
+def make_and_save( 
+    pickle_paths:dict,
+    vocabulary_decsr:str,
+    corpus:list[str], 
+    preprocessor:Preprocessor,
+    tokenMaker:TokenMaker
+):
+    vocabularyMaker = VocabularyMaker( corpus, preprocessor, tokenMaker )
+    vocabulary = vocabularyMaker.make()
 
-    dataset_option = None
-    if len( sys.argv ) >= 2:
-        dataset_option = sys.argv[ 1 ]
+    vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_decsr}.pkl"
+    PickleSaver( vocabulary_filename ).save( vocabulary )
+
+
+# RUN: python -m src.VocabularyMaker [option]
+if __name__ == "__main__":
 
     option = None
-    if len( sys.argv ) >= 3:
-        option = sys.argv[ 2 ]
+    if len( sys.argv ) >= 2:
+        option = sys.argv[ 1 ]
 
-    match dataset_option:
+    match option:
 
-        case 'arxiv':
-
+        case 'arxiv-stemm-single':
+            from .arXiv.Dataset import Dataset
             from .arXiv.settings import pickle_paths
-            from .arXiv.CorpusLoader import TitleSummaryCorpusLoader
+            make_and_save( 
+                pickle_paths,
+                vocabulary_decsr='title-summary_lower-punct-specials-stops-stemm_single',
+                corpus=Dataset().toListTitlesSummaries(),
+                preprocessor=StemmPreprocessor(),
+                tokenMaker=SingleTokenMaker()
+            )
 
-            match option:
-
-                case 'stemm-single':
-
-                    vocabularyMaker = VocabularyMaker(
-                        TitleSummaryCorpusLoader(),
-                        StemmPreprocessor(),
-                        SingleTokenMaker()
-                    )
-                    vocabulary = vocabularyMaker.make()
-
-                    vocabulary_decsr = 'title-summary_lower-punct-specials-stops-stemm_single'
-                    vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_decsr}.pkl"
-                    PickleSaver( vocabulary_filename ).save( vocabulary )
-
-                case 'lemm-single':
-
-                    vocabularyMaker = VocabularyMaker(
-                        TitleSummaryCorpusLoader(),
-                        LemmPreprocessor(),
-                        SingleTokenMaker()
-                    )
-                    vocabulary = vocabularyMaker.make()
-
-                    vocabulary_decsr = 'title-summary_lower-punct-specials-stops-lemm_single'
-                    vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_decsr}.pkl"
-                    PickleSaver( vocabulary_filename ).save( vocabulary )
-
-                case _:
-                    raise Exception( 'No valid option.' )
+        case 'arxiv-lemm-single':
+            from .arXiv.Dataset import Dataset
+            from .arXiv.settings import pickle_paths
+            make_and_save( 
+                pickle_paths,
+                vocabulary_decsr='title-summary_lower-punct-specials-stops-lemm_single',
+                corpus=Dataset().toListTitlesSummaries(),
+                preprocessor=LemmPreprocessor(),
+                tokenMaker=SingleTokenMaker()
+            )
                 
-        case 'medical':
-
+        case 'medical-lemm-single':
+            from .medical.Dataset import Dataset
             from .medical.settings import pickle_paths
-            from .medical.CorpusLoader import TitleAbstractCorpusLoader
-
-            match option:
-
-                case 'lemm-single':
-
-                    vocabularyMaker = VocabularyMaker(
-                        TitleAbstractCorpusLoader(),
-                        LemmPreprocessor(),
-                        SingleTokenMaker()
-                    )
-                    vocabulary = vocabularyMaker.make()
-
-                    vocabulary_decsr = 'title-summary_lower-punct-specials-stops-lemm_single'
-                    vocabulary_filename = f"{pickle_paths[ 'vocabularies' ]}/{vocabulary_decsr}.pkl"
-                    PickleSaver( vocabulary_filename ).save( vocabulary )
-
-                case _:
-                    raise Exception( 'No valid option.' )
+            make_and_save( 
+                pickle_paths,
+                vocabulary_decsr='title-summary_lower-punct-specials-stops-lemm_single',
+                corpus=Dataset().toListTitlesAbstracts(),
+                preprocessor=LemmPreprocessor(),
+                tokenMaker=SingleTokenMaker()
+            )
 
         case _:
-            raise Exception( 'No valid dataset option.' )
-
+            raise Exception( 'No valid option.' )
