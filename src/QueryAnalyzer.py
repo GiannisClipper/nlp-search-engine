920f52b4ff877999f657777058a2c6391b34f292
Giannis Clipper <giannisclipper@gmail.com>
2025-05-02 10:18:40 +0300

    Update QueryAnalyzer (include jina pretrained model).

--- src/QueryAnalyzer.py
+++ src/QueryAnalyzer.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 import sys
 
 import nltk
@@ -5,14 +7,24 @@ nltk.download( 'punkt_tab' ) # required by word_tokenize()
 from nltk.tokenize import word_tokenize
 
 import numpy as np
-from scipy.sparse import spmatrix
+from scipy.sparse import spmatrix, csr_matrix
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sentence_transformers import SentenceTransformer
 from .Preprocessor import Preprocessor, LemmPreprocessor, StemmPreprocessor
 from .helpers.Pickle import PickleLoader
 
-class QueryAnalyzer:
+
+class AbstractQueryAnalyzer( ABC ):
+
+    @abstractmethod
+    def analyze( self, query:str ) -> tuple[list[str],spmatrix]:
+        pass
+
+
+class QueryAnalyzerWithVectorizer( AbstractQueryAnalyzer):
 
     def __init__( self, preprocessor:Preprocessor, vectorizer:CountVectorizer|TfidfVectorizer ):
+        super().__init__()
         self._preprocessor = preprocessor
         self._vectorizer = vectorizer
 
@@ -21,6 +33,20 @@ class QueryAnalyzer:
         query_terms = list( word_tokenize( query_preprocessed[ 0 ] ) )
         query_repr = self._vectorizer.transform( query_preprocessed )
         return query_terms, query_repr
+
+
+class QueryAnalyzerWithPretrained( AbstractQueryAnalyzer):
+
+    def __init__( self, preprocessor:Preprocessor, model:SentenceTransformer ):
+        super().__init__()
+        self._preprocessor = preprocessor
+        self._model = model
+
+    def analyze( self, query:str ) -> tuple[list[str],spmatrix]:
+        query_preprocessed = self._preprocessor.transform( [ query ] )
+        query_terms = list( word_tokenize( query_preprocessed[ 0 ] ) )
+        query_repr = self._model.encode( query_preprocessed )
+        return query_terms, csr_matrix( query_repr ) # convert to spmatrix
 
 
 def queryAnalyzerFactory( option:str ):
@@ -33,7 +59,7 @@ def queryAnalyzerFactory( option:str ):
             vectorizer_descr = 'title-summary_lower-punct-specials-stops-stemm_single_count'
             vectorizer_filename = f"{pickle_paths[ 'vectorizers' ]}/{vectorizer_descr}.pkl"
             vectorizer = PickleLoader( vectorizer_filename ).load()
-            return QueryAnalyzer( preprocessor, vectorizer )
+            return QueryAnalyzerWithVectorizer( preprocessor, vectorizer )
 
         case 'arxiv-lemm-single-tfidf':
             from .arXiv.settings import pickle_paths
@@ -41,7 +67,7 @@ def queryAnalyzerFactory( option:str ):
             vectorizer_descr = 'title-summary_lower-punct-specials-stops-lemm_single_tfidf'
             vectorizer_filename = f"{pickle_paths[ 'vectorizers' ]}/{vectorizer_descr}.pkl"
             vectorizer = PickleLoader( vectorizer_filename ).load()
-            return QueryAnalyzer( preprocessor, vectorizer )
+            return QueryAnalyzerWithVectorizer( preprocessor, vectorizer )
 
         case 'medical-lemm-single-tfidf':
             from .medical.settings import pickle_paths
@@ -49,17 +75,20 @@ def queryAnalyzerFactory( option:str ):
             vectorizer_descr = 'title-summary_lower-punct-specials-stops-lemm_single_tfidf'
             vectorizer_filename = f"{pickle_paths[ 'vectorizers' ]}/{vectorizer_descr}.pkl"
             vectorizer = PickleLoader( vectorizer_filename ).load()
-            return QueryAnalyzer( preprocessor, vectorizer )
+            return QueryAnalyzerWithVectorizer( preprocessor, vectorizer )
+
+        case 'lemm-single-jina':
+            preprocessor = LemmPreprocessor()
+            model = SentenceTransformer( "jinaai/jina-embeddings-v2-base-en", trust_remote_code=True, local_files_only=True )
+            model.max_seq_length = 1024 # control your input sequence length up to 8192
+            return QueryAnalyzerWithPretrained( preprocessor, model )
 
         case _:
             raise Exception( 'queryAnalyzerFactory(): No valid option.' )
 
 
-# RUN: python -m src.QueryFinder [option]
+# RUN: python -m src.QueryAnalyzer [option]
 if __name__ == "__main__": 
-
-    from .arXiv.Dataset import Dataset
-    from .arXiv.settings import pickle_paths
 
     option = None
     if len( sys.argv ) >= 2:
@@ -78,6 +107,10 @@ if __name__ == "__main__":
             print( analyzer.analyze( query ) )
 
         case 'medical-lemm-single-tfidf':
+            analyzer = queryAnalyzerFactory( option )
+            print( analyzer.analyze( query ) )
+
+        case 'lemm-single-jina':
             analyzer = queryAnalyzerFactory( option )
             print( analyzer.analyze( query ) )
 
